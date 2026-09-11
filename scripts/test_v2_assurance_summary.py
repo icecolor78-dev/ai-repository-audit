05fd512505d79bf52e3fdc15671f73561f7b11bd
icecolor78-dev <icecolor78@gmail.com>
2026-09-11 21:57:24 +0400

    P0: regress contradicted summary preservation

--- scripts/test_v2_assurance_summary.py
+++ scripts/test_v2_assurance_summary.py
@@ -8,6 +8,12 @@ assert not s['missing_domains']
 assert s['scoring']=='NONE'
 assert all(x['state']=='PARTIAL' for x in s['domains'])
 assert all('not a PASS score' in x['statement'] for x in s['domains'])
+
+portrait['threat_model']['confidence']='CONTRADICTED'
+contradicted=summarize(portrait)
+state={x['domain']:x['state'] for x in contradicted['domains']}
+assert state['threat_model']=='CONTRADICTED'
+
 empty=summarize({})
 assert empty['complete_contract'] is False
 assert len(empty['missing_domains'])==15
